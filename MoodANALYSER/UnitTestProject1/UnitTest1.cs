54fe9ed95389fbdf8aa73a52326530160d5e23f2
Puja <pujayadav@gmail.com>
2020-10-13 18:21:48 +0530

    [puja] add . To Test whether the given mood is sad or not

---
+++ MoodANALYSER/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodANALYSER;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenMood_ReturnSad()
+        {
+            string expected = "SAD";
+            string message = "I am Sad now";
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+            string mood = moodAnalyser.AnalyseMood();
+            Assert.AreEqual(expected, mood);
+
+        }
+    }
+}
